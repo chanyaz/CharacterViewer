6992bfaacbe5643aac075ea384b8e604270861e5
sumayyah <sumayyah16.m@gmail.com>
2016-08-10 11:52:37 -0400

    Setup action bar

--- app/src/main/java/com/sumayyah/characterviewer/MainActivity.java
+++ app/src/main/java/com/sumayyah/characterviewer/MainActivity.java
@@ -1,13 +1,52 @@
 package com.sumayyah.characterviewer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setUpActionBar();
+    }
+
+    private void setUpActionBar() {
+
+        if (toolbar != null) {
+            setActionBar(toolbar);
+            getActionBar().setDisplayHomeAsUpEnabled(false);
+            getActionBar().setDisplayShowTitleEnabled(true);
+            getActionBar().setElevation(7);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_toggle) {
+            //toggle();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

f9d30e1a7a2e357efe13b1c76b8b3e0218bfd01c
sumayyah <sumayyah16.m@gmail.com>
2017-02-24 15:15:57 -0500

    Add basic data binding (#3)
    Use Android’s DataBindingUtil to experiment with very basic data binding in fragment views
    
    * Add data binding dependencies
    * Basic layout notation for xml files
    * Add basic bindings

--- app/src/main/java/com/sumayyah/characterviewer/main/Adapters/CharacterListAdapter.java
+++ app/src/main/java/com/sumayyah/characterviewer/main/Adapters/CharacterListAdapter.java
@@ -1,6 +1,7 @@
 package com.sumayyah.characterviewer.main.Adapters;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.graphics.Point;
 import android.support.v7.widget.RecyclerView;
 import android.view.Display;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.sumayyah.characterviewer.R;
+import com.sumayyah.characterviewer.databinding.ListFragmentItemLayoutBinding;
 import com.sumayyah.characterviewer.main.ListFragment;
 
 import com.sumayyah.characterviewer.main.Model.Character;
@@ -23,37 +25,34 @@ import com.sumayyah.characterviewer.main.Managers.DataManager;
 /**
  * Created by sumayyah on 8/10/16.
  */
-public class CharacterListAdapter extends RecyclerView.Adapter<CharacterListAdapter.ViewHolder> {
+public class CharacterListAdapter extends RecyclerView.Adapter<CharacterListAdapter.ViewHolder>  {
 
-    private ListFragment.ListItemClickListener listItemClickListener;
     public boolean isList;
     private Context context;
+    private ListFragment.ListItemClickListener listItemClickListener;
 
     public CharacterListAdapter(ListFragment.ListItemClickListener listItemClickListener, Context context) {
-        this.listItemClickListener = listItemClickListener;
         this.context = context;
+        this.listItemClickListener = listItemClickListener;
         isList = true;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        private ImageView profilePic;
-        private TextView characterName;
-        private LinearLayout cardContentLayout;
+        private ListFragmentItemLayoutBinding binding;
 
-        public ViewHolder(View itemView) {
-            super(itemView);
-            profilePic = (ImageView) itemView.findViewById(R.id.profilePic);
-            characterName = (TextView) itemView.findViewById(R.id.characterName);
-            cardContentLayout = (LinearLayout) itemView.findViewById(R.id.cardContentLayout);
+        public ViewHolder(ListFragmentItemLayoutBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+            this.binding.setViewHolder(this);
         }
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_fragment_item_layout, parent, false);
-        return new ViewHolder(view);
+        ListFragmentItemLayoutBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.list_fragment_item_layout, parent, false);
+        return new ViewHolder(binding);
     }
 
     @Override
@@ -66,17 +65,17 @@ public class CharacterListAdapter extends RecyclerView.Adapter<CharacterListAdap
         //If grid view, add a profile image
         if(!isList) {
             if(c.getImageURL().length() > 0) {
-                Picasso.with(context).load(c.getImageURL()).placeholder(R.drawable.placeholder_profile_pic).into(holder.profilePic);
+                Picasso.with(context).load(c.getImageURL()).placeholder(R.drawable.placeholder_profile_pic).into(holder.binding.profilePic);
             } else {
-                Picasso.with(context).load(R.drawable.placeholder_profile_pic).placeholder(R.drawable.ic_view_grid).into(holder.profilePic);
+                Picasso.with(context).load(R.drawable.placeholder_profile_pic).placeholder(R.drawable.ic_view_grid).into(holder.binding.profilePic);
             }
         } else {
-            holder.profilePic.setVisibility(View.GONE);
+            holder.binding.profilePic.setVisibility(View.GONE);
         }
 
         //Set common information
-        holder.characterName.setText(c.getName());
-        holder.cardContentLayout.setOnClickListener(new View.OnClickListener() {
+        holder.binding.characterName.setText(c.getName());
+        holder.binding.cardContentLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 listItemClickListener.onListItemSelected(holder.getAdapterPosition());
